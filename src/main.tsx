f44018f5c41950bf18dffaa312333ee887cf1be1
omer ahmed <omerahm69@gmail.com>
2026-01-03 22:08:08 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import App from "./App.tsx";
 import "./i18n";
 import "./index.css";
 
-
-/*createRoot(document.getElementById("root")!).render(<App />);*/
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <LanguageProvider>
@@ -16,23 +14,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   </React.StrictMode>
 );
 
-/*
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import { LanguageProvider } from "@/i18n/LanguageContext";
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Root element not found");
-}
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  </React.StrictMode>
-);*/
